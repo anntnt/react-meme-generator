db77c58230f01702bba39aac8363b88b3861cd1c
An Tran <anpjure@gmail.com>
2024-09-19 18:16:05 +0200

    Correct ESLint errors

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
-import React, { useEffect, useState } from 'react';
 import WorkingMeme from './containers/WorkingMeme';
 
 const App = () => {
-  const [allMemes, setAllMemes] = useState([]);
-
-  useEffect(() => {
-    fetch('https://api.memegen.link/images?filter=cat')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('data: ' + data);
-        setAllMemes(data);
-      })
-      .catch((error) => console.log(error.message));
-  }, []); // Empty dependency array ensures it runs once after component mounts
-
   return (
     <div className="App">
       <WorkingMeme />
